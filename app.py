99bcf8c659d41680220d0c11ee4a7c97a7d3c30d
Nayan Samadhiya <samadhiyanayan7@gmail.com>
2026-06-24 22:11:51 +0530

    devloper A code

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ print("hellooo")
 # •	Naya commit (same file) → git add + git commit + git push
 #•	Naya commit (nayi file) → same hi — git add + git commit + git push
 #git branch aur git remote add origin — dono sirf ek baar. Chahe kitni bhi files ho.
+
+print("nayan") # devloper a ka code
